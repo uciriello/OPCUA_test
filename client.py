1059306da0281cdaf1b134ed160a30395b1bdb5d
Umberto Ciriello <umberto.ciriello@quinck.io>
2021-04-23 12:59:52 +0200

    added mqtt

--- client.py
+++ client.py
@@ -1,24 +1,38 @@
 from opcua import Client
+import paho.mqtt.client as mqtt
 import time
+import json
 
-url = "opc.tcp://localhost:4840"
-
+url = "opc.tcp://192.168.1.17:4840"
 client = Client(url)
-
 client.connect()
-print("Client Connected")
+print("OPC UA Client connected")
 
+iot_hub = "demo.thingsboard.io"
+port = 1883
+username = "whohDlx7wkEXXuoWim6Y"  
+password = ""
+topic = "v1/devices/me/telemetry"
+
+iot_hub_client = mqtt.Client()
+iot_hub_client.username_pw_set(username, password)
+iot_hub_client.connect(iot_hub, port)
+print("Connected to IOT hub")
+
+data = dict()
 while True:
-    TempNode = client.get_node("ns=2;i=2")
-    Temperature = TempNode.get_value()
-    print(Temperature)
+    try:
+        temp = client.get_node("ns=2;i=2")
+        press = client.get_node("ns=2;i=3")
+        temperature = temp.get_value()
+        pressure = press.get_value()
+        print(temperature, pressure)
 
-    PressNode = client.get_node("ns=2;i=3")
-    Pressure = PressNode.get_value()
-    print(Pressure)
+        data["temperature"] = int(temperature)
+        data["pressure"] = int(pressure)
+        data_out = json.dumps(data)
+        iot_hub_client.publish(topic, data_out, 0)
 
-    TimeNode = client.get_node("ns=2;i=4")
-    CurrentTime = TimeNode.get_value()
-    print(CurrentTime)
-
-    time.sleep(0.1)
+        time.sleep(2)
+    except Exception as e:
+        print(e)
